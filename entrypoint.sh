9f7c84ce6a4f5a4f0523d087d72c8dc2c26b611b
JesusZapata <jesus@vauxoo.com>
2017-07-18 16:05:13 -0400

    [FIX] entrypoint.sh: Delete unnecesary configuration
    And add if for avoid the error when the used is already create

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,22 +1,20 @@
 #!/usr/bin/env bash
 set -e
 
-# Create the user with ssh access
-useradd -d "/home/${SSH_USER}" -m -s "/bin/bash" "${SSH_USER}"
-mkdir -p /home/${SSH_USER}/.ssh/
+if ! grep ${SSH_USER} /etc/passwd ; then
+	# Create the user with ssh access
+	useradd -d "/home/${SSH_USER}" -m -s "/bin/bash" "${SSH_USER}"
+	mkdir -p /home/${SSH_USER}/.ssh/
 
-# Configure ssh_config
-mkdir -p /var/run/sshd
-if ! grep "PasswordAuthentication no" /etc/ssh/ssh_config ; then
-	echo "Port 22" >> /etc/ssh/ssh_config
-	echo "PasswordAuthentication no" >> /etc/ssh/ssh_config
-	echo "AuthenticationMethods 'publickey'" >> /etc/ssh/ssh_config
-	echo "PermitRootLogin no" >> /etc/ssh/ssh_config
-	echo "AllowUsers ${SSH_USER}" >> /etc/ssh/ssh_config
+	# Configure ssh_config
+	mkdir -p /var/run/sshd
+	echo "PasswordAuthentication no" >> /etc/ssh/sshd_config
+	echo "PermitRootLogin no" >> /etc/ssh/sshd_config
+	echo "AllowUsers ${SSH_USER}" >> /etc/ssh/sshd_config
+
+	# Copy the authorized_keys
+	touch /home/${SSH_USER}/.ssh/authorized_keys
+	echo "$SSH_KEY" >> /home/${SSH_USER}/.ssh/authorized_keys
 fi
-
-# Copy the authorized_keys
-touch /home/${SSH_USER}/.ssh/authorized_keys
-echo "$SSH_KEY" >> /home/${SSH_USER}/.ssh/authorized_keys
 
 exec "$@"
